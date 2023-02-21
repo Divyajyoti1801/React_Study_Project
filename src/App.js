92889113ba4609b6555cb24c135b79bcc6c6fbde
Divyajyoti <divyajyoti182000@gmail.com>
2023-02-21 21:49:22 +0530

    Edited Component and setup understanding routing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import CategoryItem from './components/category-item/category-item.components';
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Router/home/home.router";
 //This is where all routes are present this is like main function of project.
 const App = () => {
-  const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
-
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-      <CategoryItem category={category} key={category.id} />
-      ))}
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
