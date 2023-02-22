0c3d45c1e69904967f2808f3b5296b3ce0dd6929
Divyajyoti <divyajyoti182000@gmail.com>
2023-02-22 21:28:26 +0530

    navigation component,routing, Outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Router/home/home.router";
 //This is where all routes are present this is like main function of project.
+
+
+import Navigation from "./Router/navigation/navigation.router";
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
